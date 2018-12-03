4941230078b45155a841cf7ef51d4592be152b50
chung_marco <marco_97m@yahoo.com.hk>
2018-12-03 12:12:42 +0800

    ..

--- server.js
+++ server.js
@@ -585,7 +585,7 @@ app.post('/create', function (req, res) {
 	}
 })
 
-app.get('api/name/:name', function (req, res) {
+app.get('api/restaurant/name/:name', function (req, res) {
 	var criteria = {name : req.params.name}
 
 	MongoClient.connect(mongourl, function (err, db) {
@@ -601,7 +601,7 @@ app.get('api/name/:name', function (req, res) {
 	})
 })
 
-app.get('/api/borough/:borough', function (req, res) {
+app.get('/api/restaurant/borough/:borough', function (req, res) {
 	var criteria = {borough : req.params.borough}
 
 	MongoClient.connect(mongourl, function (err, db) {
@@ -617,7 +617,7 @@ app.get('/api/borough/:borough', function (req, res) {
 	})
 })
 
-app.get('/api/cuisine/:cuisine', function (req, res) {
+app.get('/api/restaurant/cuisine/:cuisine', function (req, res) {
 	var criteria = {cuisine : req.params.cuisine}
 
 	MongoClient.connect(mongourl, function (err, db) {
